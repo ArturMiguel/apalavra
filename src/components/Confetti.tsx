95252318d034d95c00e7e8483cba7af3d8c8994a
ArturMiguel <arturmrocha1@gmail.com>
2022-02-15 20:41:26 -0300

    breaking_change: Migrando UI para o framework chakra-ui

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -1,12 +1,17 @@
-import ReactConfetti from "react-confetti";
+import ReactCanvasConfetti from "react-canvas-confetti";
 
-export default function Confetti() {
+export default function Confetti({ fire }) {
   return (
-    <ReactConfetti
-      width={window.innerWidth}
-      height={window.innerHeight}
-      tweenDuration={1}
-      recycle={false}
-    ></ReactConfetti>
+    <div>
+      <ReactCanvasConfetti fire={fire} style={{
+        position: "fixed",
+        pointerEvents: "none",
+        width: "100%",
+        height: "100%",
+        zIndex: 2000,
+        top: 0,
+        left: 0
+      }} />
+    </div>
   )
 }

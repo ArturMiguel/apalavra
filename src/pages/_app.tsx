229ca5d01f56b874ce45bb106352da8b9382efaf
ArturMiguel <arturmrocha1@gmail.com>
2022-02-19 19:43:11 -0300

    fix: Correção SSR para atribuir useEffect ao useLayoutEffect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,13 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import Head from 'next/head';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (typeof document === "undefined") {
+    React.useLayoutEffect = React.useEffect;
+  }
+
   return (
     <>
       <Head>
